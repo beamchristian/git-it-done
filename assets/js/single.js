76d76305229a4098dccc641431d2658158865295
beamchristian <beamchristian@yahoo.com>
2021-09-23 16:40:36 -0400

    Error Handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,29 @@
+'use strict';
 const issueContainerEl = document.querySelector("#issues-container");
 const limitWarningEl = document.querySelector("#limit-warning");
 const repoNameEl = document.querySelector("#repo-name");
 
-const getRepoIssues = function(repo) {
-    let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+const getRepoName = function () {
+    // grab repo name from url query string
+    let queryString = document.location.search;
+    let repoName = queryString.split("=")[1];
 
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+
+};
+
+const getRepoIssues = function(repo) {
+    // format the github api url
+    const apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -17,33 +36,28 @@ const getRepoIssues = function(repo) {
                     displayWarning(repo);
                 }
             });
+        } else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
-        else {
-            alert("There was a problem with your request!")
-        }
-    });
-    
+    });  
 };
 
-const getRepoName = function () {
-    let queryString = document.location.search;
-    let repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-
-}
 
 const displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
+
+    // loop over given issues
     for (let i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         let issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
+
         // create span to hold issue title
         let titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
@@ -63,6 +77,7 @@ const displayIssues = function(issues) {
 
     // append to container
     issueEl.appendChild(typeEl);
+    // append to the dom
     issueContainerEl.appendChild(issueEl);
     }
 }
